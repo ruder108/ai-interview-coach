54c8479f7f95fd7ab8375b787053222c87c21a64
ruder <rudermittal01@gmail.com>
2026-06-11 17:26:17 +0530

    Added multi-questions interview system with scoring

--- app.py
+++ app.py
@@ -31,7 +31,8 @@ def interview():
         username=username,
         role=role,
         question=question,
-        question_index=0
+        question_index=0,
+        total_score=0
         )
     
 @app.route("/feedback", methods=["POST"])
@@ -42,22 +43,40 @@ def feedback():
     role = request.form["role"]
     question_index = int(request.form["question_index"])
     next_index = question_index + 1
+    print(request.form)
+    total_score= int(request.form["total_score"])
+    if len(answer) > 30:
+        score = 8
+    else:
+        score = 4
+
+    total_score += score
     
     if next_index < len(questions[role]):
         next_question = questions[role][next_index] 
+    
         return render_template(
             "interview.html",
             username=username,
             role=role,
             question=next_question,
-            question_index=next_index
+            question_index=next_index,
+            total_score=total_score
             )
+    if total_score >= 20:
+        performance = "excelent"
+    elif total_score >= 12:
+        performance = "Good"
+    else:
+        performance = "Needs Improvement"
+          
     return render_template(
         "feedback.html",
         username=username, 
         question=question, 
         answer=answer, 
-        score=8,
+        score=total_score,
+        performance=performance,
         feedback_text="Interview Completed!"
         )    
 
